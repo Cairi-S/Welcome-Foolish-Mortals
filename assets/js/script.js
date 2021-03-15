5c12c4517c46a98a8d811b1e0ebfe6f521158c5b
Cairi-S <cairi.swainson@gmail.com>
2021-03-15 14:18:44 +0000

    Add variables, event listener and function for modal display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-function rulesModal() {
+// Get navbar modal element
+var navbarModal = document.getElementById("ruleModal");
 
-};
+// Get button that opens modal
+var openBtn = document.getElementById("modalNav");
 
-function selectCharacter() {
+// Get button that closes button
+var closeBtn = document.getElementsByClassName("closeNavModal")[0];
 
-};
+// Event listening for click
+openBtn.addEventListener("click", openModal);
+
+function openModal() {
+    navbarModal.style.display = "block";
+}

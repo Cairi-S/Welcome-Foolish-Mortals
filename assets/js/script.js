94b98eed0478cd5eeb91baf718913e86684cf4c4
Cairi-S <cairi.swainson@gmail.com>
2021-03-25 16:31:48 +0000

    Remove un-needed code for workflow purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,6 @@ let openBtn = document.getElementById("modalNav");
 // Get button that closes modal
 let closeBtn = document.getElementsByClassName("closeNavModal")[0];
 
-// Get button that starts the game
-let startBtn = document.getElementById("startBtn");
-
-// Get game buttons of different colors
-const tealBtn = document.querySelector(".btn-teal");
-const whiteBtn = document.querySelector(".btn-white");
-const purpleBtn = document.querySelector(".btn-purple");
-const greyBtn = document.querySelector(".btn-grey");
-
 
 // Event listening for opening click on modal
 openBtn.addEventListener("click", openModal);
@@ -45,6 +36,13 @@ function clickOutsideModal(event) {
     }
 }
 
+// Toggle mute icon on and off 
+$('#toggleMute').click(function() {
+  $("toggleSound").toggle('1000');
+  $("i",this).toggleClass('fas fa-volume-up fas fa-volume-mute');
+});
+
+
 // Changes turn counter to 0 on click 
 $("#startBtn").click(function() {
     $("#turnsTaken").text("0");
@@ -52,10 +50,4 @@ $("#startBtn").click(function() {
     $(".btn-white").css("background-color", "#edeffb");
     $(".btn-purple").css("background-color", "#6c53a4");
     $(".btn-grey").css("background-color", "#040000");
-});
-
-// Toggle mute icon on and off 
-$('#toggleMute').click(function() {
-  $("toggleSound").toggle('1000');
-  $("i",this).toggleClass('fas fa-volume-up fas fa-volume-mute');
 });

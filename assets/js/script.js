eb73474c6e276233bdbd252bf071736d41a5d629
Cairi-S <cairi.swainson@gmail.com>
2021-03-28 19:04:57 +0000

    Add click function for user clicks on buttons, general housekeeping

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let playerSequence = [];
 let flash;
 let turnSpeed;
 let turn;
+let playerTurn = false;
 let sound = true;
 let good;
 let win;
@@ -90,15 +91,22 @@ function beginGame() {
     turnSpeed = setInterval(gamePlay, 800);
 }
 
+// Checks whether it's the players turn
 function gamePlay() {
+    playerTurn = false
+
+    // When the number of flashes is equal to number of turns interval cleared the computers turn is over and the game is reset for the user to play
     if (flash == turn) {
         clearInterval(turnSpeed);
         computerPlay = false;
         originalColor();
+        playerTurn = true;
     }
 
+    // If it is the computers turn a time is set for the flashes
     if (computerPlay) {
         setTimeout(() => {
+            // Links the sequence numbers to the buttons, answering functions below
             if (sequence[flash] == 1) teal();
             if (sequence[flash] == 2) white();
             if (sequence[flash] == 3) purple();
@@ -108,6 +116,7 @@ function gamePlay() {
     }
 }
 
+// Answering functions for linking to sequence numbers
 function teal() {
     if (sound) {
       tealAudio.play();
@@ -140,6 +149,66 @@ function grey() {
     $(".btn-grey").css("background-color", "#040000");
 }
 
+// Event Listeners for buttons during gameplay.  Checks if it's the players turn to allow clicks.
+// iterates through the player sequence, checks if the player was correct and then calls that futtons function.
+// If not a win then after a set amount of time the color returns to the original color.
+$(".btn-teal").click(function() {
+      if (playerTurn) {
+        playerSequence.push(1);
+        checkAnswer();
+        teal();
+        if(!win) {
+            setTimeout(() => {
+                originalColor();
+            }, 300);
+        console.log('clicked')
+        }
+      } 
+    });
+
+    $(".btn-white").click(function() {
+          if (playerTurn) {
+        playerSequence.push(2);
+        checkAnswer();
+        white();
+        if(!win) {
+            setTimeout(() => {
+                originalColor();
+            }, 300);
+        console.log('clicked')
+        }
+      } 
+    });
+
+
+    $(".btn-purple").click(function() {
+      if (playerTurn) {
+        playerSequence.push(3);
+        checkAnswer();
+        purple();
+        if(!win) {
+            setTimeout(() => {
+                originalColor();
+            }, 300);
+        console.log('clicked')
+        }
+      } 
+    });
+
+    $(".btn-grey").click(function() {
+      if (playerTurn) {
+        playerSequence.push(4);
+        checkAnswer();
+        grey();
+        if(!win) {
+            setTimeout(() => {
+                originalColor();
+            }, 300);
+        console.log('clicked')
+        }
+      } 
+    });
+
 // Makes all buttons opacity change
 function originalColor() {
     $(".btn-teal").css("background-color", "rgba(78, 160, 174, 0.5)");
@@ -148,34 +217,10 @@ function originalColor() {
     $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
 }
 
+// Makes all buttons return to full opacity
 function flashColor() {
     $(".btn-teal").css("background-color", "#4ea0ae");
     $(".btn-white").css("background-color", "#edeffb");
     $(".btn-purple").css("background-color", "#6c53a4");
     $(".btn-grey").css("background-color", "#040000");
 }
-
-// Test to check audio linked successfully
-$(".btn-teal").click(function() {
-      console.log('clicked')
-      $(".btn-teal").css("background-color", "rgba(78, 160, 174, 0.5)");
-      tealAudio.play();
-    });
-
-    $(".btn-white").click(function() {
-      console.log('clicked')
-      $(".btn-white").css("background-color", "rgba(237, 239, 251, 0.5)");
-      whiteAudio.play();
-    });
-
-    $(".btn-purple").click(function() {
-      console.log('clicked')
-      $(".btn-purple").css("background-color", "rgba(108, 83, 164, 0.5)");
-      purpleAudio.play();
-    });
-
-    $(".btn-grey").click(function() {
-      console.log('clicked')
-      $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
-      greyAudio.play();
-    });

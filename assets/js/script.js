c2182e6724f1a743954f7006b65987673eb32ef2
Cairi-S <cairi.swainson@gmail.com>
2021-03-28 16:32:19 +0000

    add variables for sound, new function for game linking flashes to sequence

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,15 @@ let playerSequence = [];
 let flash;
 let turnSpeed;
 let turn;
+let sound = true;
 let good;
 let win;
 
+// Audio files
+const tealAudio = new Audio("assets/audio/AEighth.mp3");
+const whiteAudio = new Audio("assets/audio/EEighth.mp3");
+const purpleAudio = new Audio("assets/audio/DEighth.mp3");
+const greyAudio = new Audio("assets/audio/BEighth.mp3");
 
 
 // Gets different html elements
@@ -80,7 +86,56 @@ function beginGame() {
     
     computerPlay = true;
 
-    turnSpeed = setInterval(gamePlay, 1000);
+    turnSpeed = setInterval(gamePlay, 500);
+}
+
+function gamePlay() {
+    if (flash == turn) {
+        clearInterval(turnSpeed);
+        computerPlay = false;
+    }
+
+    if (computerPlay) {
+        setTimeout(() => {
+            if (sequence[flash] == 1) teal();
+            if (sequence[flash] == 2) white();
+            if (sequence[flash] == 3) purple();
+            if (sequence[flash] == 4) grey();
+            flash++;
+        }, 300);
+    }
+}
+
+function teal() {
+    if (sound) {
+      tealAudio.play();
+    }
+    sound = true;
+    $(".btn-teal").css("background-color", "#4ea0ae");
+}
+
+function white() {
+    if (sound) {
+    whiteAudio.play();
+    }
+    sound = true;
+    $(".btn-white").css("background-color", "#edeffb");
+}
+
+function purple() {
+    if (sound) {
+      purpleAudio.play();
+    }
+    sound = true;
+    $(".btn-purple").css("background-color", "#6c53a4");
+}
+
+function grey() {
+    if (sound) {
+      greyAudio.play();
+    }
+    sound = true;
+    $(".btn-grey").css("background-color", "#040000");
 }
 
 // Makes all buttons opacity change
@@ -90,3 +145,35 @@ function flashColor() {
     $(".btn-purple").css("background-color", "rgba(108, 83, 164, 0.5)");
     $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
 }
+
+function originalColor() {
+    $(".btn-teal").css("background-color", "#4ea0ae");
+    $(".btn-white").css("background-color", "#edeffb");
+    $(".btn-purple").css("background-color", "#6c53a4");
+    $(".btn-grey").css("background-color", "#040000");
+}
+
+// Test to check audio linked successfully
+$(".btn-teal").click(function() {
+      console.log('clicked')
+      $(".btn-teal").css("background-color", "rgba(78, 160, 174, 0.5)");
+      tealAudio.play();
+    });
+
+    $(".btn-white").click(function() {
+      console.log('clicked')
+      $(".btn-white").css("background-color", "rgba(237, 239, 251, 0.5)");
+      whiteAudio.play();
+    });
+
+    $(".btn-purple").click(function() {
+      console.log('clicked')
+      $(".btn-purple").css("background-color", "rgba(108, 83, 164, 0.5)");
+      purpleAudio.play();
+    });
+
+    $(".btn-grey").click(function() {
+      console.log('clicked')
+      $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
+      greyAudio.play();
+    });

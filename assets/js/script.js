0ad3434ca19838b753e2609df856f3f183b14967
Cairi-S <cairi.swainson@gmail.com>
2021-04-02 10:08:43 +0000

    rename variables to be more succinct, edit throughout document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 // Game variables
-let sequence = [];
+let computerSequence = [];
 let playerSequence = [];
-let flash;
-let turnSpeed;
+
+let flashCounter;
+//let flashInterval = 1000; CHECK HOW TO IMPLEMENT THIS
+
+let intervalRef;
+
 let turn;
-let highscore;
-let computerPlay;
-let playerTurn = false;
-let sound = true;
-let correct;
-let win;
+
+let highestScoreCounter;
+let isComputerTurn;
+let isPlayerTurn = false;
+let isMuted = false;
+
+let hasSequenceMatched;
+let hasPlayerWon;
 
 // Audio files
 const tealAudio = new Audio("assets/audio/Ashort.mp3");
@@ -48,10 +54,10 @@ function startGame() {
 // Resets the game clearing sequences, intervals and turns.  Reset button is hidden and start button appears prompting player to restart
 
 function resetGame() {
-  sequence = [];
+  computerSequence = [];
   playerSequence = [];
   turn = 0;
-  clearInterval(turnSpeed);
+  clearInterval(intervalRef);
   clearTimeout();
   $("#turnsTaken").text("-");
   resetButton.classList.add("hide-content");
@@ -60,64 +66,63 @@ function resetGame() {
 
 // Starts the first turn resetting the game, creating the randomised sequence and passing to the computer.
 function beginGame() {
-  win = false;
-  sequence = [];
+  hasPlayerWon = false;
+  computerSequence = [];
   playerSequence = [];
-  flash = 0;
-  turnSpeed = 0;
+  flashCounter = 0;
+  intervalRef = 0;
   turn = 1;
   turnCounter.innerHTML = 1;
-  highscore = 1;
+  highestScoreCounter = 1;
   highScore.innerHTML = 1;
-  correct = true;
+  hasSequenceMatched = true;
 
-  for (let i = 0; i < 5; i++) {
-    sequence.push(Math.floor(Math.random() * 4) + 1);
+  for (let i = 0; i < 10; i++) {
+    computerSequence.push(Math.floor(Math.random() * 4) + 1);
   }
-  console.log(sequence);
 
-  computerPlay = true;
+  isComputerTurn = true;
 
-  turnSpeed = setInterval(gamePlay, 800);
+  intervalRef = setInterval(gamePlay, 800); //sets how long it takes for game to start
 }
 
 // Checks whether it's the players turn
 function gamePlay() {
-  playerTurn = false;
+  isPlayerTurn = false;
 
   // When the number of flashes is equal to number of turns interval cleared the computers turn is over and the game is reset for the user to play
-  if (flash === turn) {
-    clearInterval(turnSpeed);
-    computerPlay = false;
+  if (flashCounter === turn) {
+    clearInterval(intervalRef);
+    isComputerTurn = false;
     originalColor();
-    playerTurn = true;
+    isPlayerTurn = true;
   }
 
   // If it is the computers turn a time is set for the flashes
-  if (computerPlay) {
+  if (isComputerTurn) {
     originalColor();
     setTimeout(() => {
       // Links the sequence numbers to the buttons, answering functions below
-      if (sequence[flash] === 1) {
-        playColorAudio(flashTeal, "#4ea0ae", tealAudio);
+      if (computerSequence[flashCounter] === 1) {
+        playColorAudio(flashTeal, tealAudio);
       }
-      if (sequence[flash] === 2) {
-        playColorAudio(flashWhite, "#edeffb", whiteAudio);
+      if (computerSequence[flashCounter] === 2) {
+        playColorAudio(flashWhite, whiteAudio);
       }
-      if (sequence[flash] === 3) {
-        playColorAudio(flashPurple, "#6c53a4", purpleAudio);
+      if (computerSequence[flashCounter] === 3) {
+        playColorAudio(flashPurple, purpleAudio);
       }
-      if (sequence[flash] === 4) {
-        playColorAudio(flashGrey, "#040000", greyAudio);
+      if (computerSequence[flashCounter] === 4) {
+        playColorAudio(flashGrey, greyAudio);
       }
-      flash++;
-    }, 200);
+      flashCounter++;
+    }, 200); //length of flash on computer turn
   }
 }
 
-function playColorAudio(colorFunction, colorHex, varAudio) {
+function playColorAudio(colorFunction, varAudio) {
   colorFunction();
-  if (sound) {
+  if (!isMuted) {
     varAudio.play();
   }
 }
@@ -126,114 +131,112 @@ function playColorAudio(colorFunction, colorHex, varAudio) {
 // iterates through the player sequence, checks if the player was correct and then calls that futtons function.
 // If not a win then after a set amount of time the color returns to the original color.
 $(".btn-teal").click(function () {
-  if (playerTurn) {
+  if (isPlayerTurn) {
     playerSequence.push(1);
     checkAnswer();
-    playColorAudio(flashTeal, "#4ea0ae", tealAudio);
-    if (!win) {
+    playColorAudio(flashTeal, tealAudio);
+    }
+    if (!hasPlayerWon) {
       setTimeout(() => {
         originalColor();
-      }, 300);
-      console.log("clicked");
-    }
+      }, 300); //length of flash on player click
   }
 });
 
 $(".btn-white").click(function () {
-  if (playerTurn) {
+  if (isPlayerTurn) {
     playerSequence.push(2);
     checkAnswer();
-    playColorAudio(flashWhite, "#edeffb", whiteAudio);
-    if (!win) {
+    playColorAudio(flashWhite, whiteAudio);
+    }
+    if (!hasPlayerWon) {
       setTimeout(() => {
         originalColor();
-      }, 300);
-      console.log("clicked");
-    }
+      }, 300); //length of flash on player click
   }
 });
 
 $(".btn-purple").click(function () {
-  if (playerTurn) {
+  if (isPlayerTurn) {
     playerSequence.push(3);
     checkAnswer();
-    playColorAudio(flashPurple, "#6c53a4", purpleAudio);
-    if (!win) {
+    playColorAudio(flashPurple, purpleAudio);
+    
+    }
+    if (!hasPlayerWon) {
       setTimeout(() => {
         originalColor();
-      }, 300);
-      console.log("clicked");
-    }
+      }, 300); //length of flash on player click
   }
 });
 
 $(".btn-grey").click(function () {
-  if (playerTurn) {
+  if (isPlayerTurn) {
     playerSequence.push(4);
     checkAnswer();
-    playColorAudio(flashGrey, "#040000", greyAudio);
-    if (!win) {
+    playColorAudio(flashGrey, greyAudio);
+    
+    }
+    if (!hasPlayerWon) {
       setTimeout(() => {
         originalColor();
-      }, 300);
-      console.log("clicked");
-    }
+      }, 300); //length of flash on player click
   }
 });
 
 // Checks whether the player answer during gameplay
 function checkAnswer() {
   // Checks if the player sequence equals the computer sequence
-  if (playerSequence[playerSequence.length - 1] !== sequence[playerSequence.length - 1]) correct = false;
+  if (playerSequence[playerSequence.length - 1] !== computerSequence[playerSequence.length - 1]) hasSequenceMatched = false;
 
   // checks if the player sequence has met the win game criteria and calls game win function
-  if (playerSequence.length == 3 && correct) {
+  if (playerSequence.length == 10 && hasSequenceMatched) {
     winGame();
   }
 
   // If the player sequence does not match the computer sequence
-  if (!correct) {
+  if (!hasSequenceMatched) {
     flashAll();
     turnCounter.innerHTML = "NO!";
     setTimeout(() => {
       turnCounter.innerHTML = turn;
       originalColor();
-    }, 800);
+    }, 800); //sets the time for the turn counter and buttons to return to original color
 
-    sound = false;
+    isMuted = false;
     
     loseModalTrigger();
   }
 
   // If the player is correct in their sequence but has not met the win criteria
-  if (turn == playerSequence.length && correct && !win) {
+  if (turn == playerSequence.length && hasSequenceMatched && !hasPlayerWon) {
     turn++;
-    highscore++;
+    highestScoreCounter++;
     playerSequence = [];
-    computerPlay = true;
-    flash = 0;
+    isComputerTurn = true;
+    flashCounter = 0;
     turnCounter.innerHTML = turn;
-    highScore.innerHTML = highscore;
-    turnSpeed = setInterval(gamePlay, 800);
+    highScore.innerHTML = highestScoreCounter;
+    intervalRef = setInterval(gamePlay, 800); //sets speed of note repetition
   }
 }
 
 // Makes all buttons return to full opacity
 
 function flashTeal() {
-  $(".btn-teal").css("background-color", "#4ea0ae");
+  $(".btn-teal").css("background-color", "rgba(78, 160, 174, 0.5)");
 }
 
-function flashWhite() {
-  $(".btn-white").css("background-color", "#edeffb");
+function flashWhite() { 
+  $(".btn-white").css("background-color", "rgba(237, 239, 251, 0.5)");
 }
 
-function flashPurple() {
-  $(".btn-purple").css("background-color", "#6c53a4");
+function flashPurple() { 
+  $(".btn-purple").css("background-color", "rgba(108, 83, 164, 0.5)");
 }
 
 function flashGrey() {
-  $(".btn-grey").css("background-color", "#040000");
+  $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
 }
 
 function flashAll() {
@@ -245,17 +248,17 @@ function flashAll() {
 
 // Makes all buttons opacity change
 function originalColor() {
-  $(".btn-teal").css("background-color", "rgba(78, 160, 174, 0.5)");
-  $(".btn-white").css("background-color", "rgba(237, 239, 251, 0.5)");
-  $(".btn-purple").css("background-color", "rgba(108, 83, 164, 0.5)");
-  $(".btn-grey").css("background-color", "rgba(4, 0, 0, 0.5)");
+  $(".btn-teal").css("background-color", "#4ea0ae");
+  $(".btn-white").css("background-color", "#edeffb");
+  $(".btn-purple").css("background-color", "#6c53a4");
+  $(".btn-grey").css("background-color", "#040000");
 }
 
 function winGame() {
   flashAll();
   turnCounter.innerHTML = "Win";
-  playerTurn = false;
-  win = true;
+  isPlayerTurn = false;
+  hasPlayerWon = true;
   winModalTrigger();
 }
 

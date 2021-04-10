bb1ca803a108811ea7c4742fd6e0b584a771245e
Cairi-S <cairi.swainson@gmail.com>
2021-04-10 13:13:46 +0000

    Condense closing functions/listeners, remove lose modal, move start/reset buttons to script.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,33 +1,24 @@
 // With thanks to Travery Media for their guide to creating modals
 // Get navbar modal element
-let navbarModal = document.getElementById("ruleModal");
+let navbarModal = document.getElementById("navbarModal");
 let scoreModal = document.getElementById("scoreModal");
-// let loseModal = document.getElementById("loseModal");
 
 // Get button that opens modal
 let openBtn = document.getElementById("modalNav");
 
 // Get buttons that close modals
 let closeBtn = document.getElementsByClassName("closeNavModal")[0];
-let closeWinBtn = document.getElementsByClassName("closeScoreModal")[0];
-// let closeLoseBtn = document.getElementsByClassName("closeLoseModal")[0];
+//let closeScoreBtn = document.getElementsByClassName("closeScoreModal")[0];
 
 // Event listening for opening click on modal
 openBtn.addEventListener("click", openModal);
 
 // Event listening for closing click on modals
 closeBtn.addEventListener("click", closeModal);
-closeWinBtn.addEventListener("click", closeScoreModal);
-// closeLoseBtn.addEventListener("click", closeLoseModal);
+//closeScoreBtn.addEventListener("click", closeScoreModal);
 
 // Event listening for closing click outside of modal box
 window.addEventListener("click", clickOutsideModal);
-
-//Event listening for click on start button
-startButton.addEventListener("click", startGame);
-
-//Event listening for click on reset button
-resetButton.addEventListener("click", resetGame);
 
 // Function for opening modal
 function openModal() {
@@ -40,10 +31,6 @@ function highScoreModalTrigger() {
     setNewHighScore();
 }
 
-/*function loseModalTrigger() {
-    loseModal.style.display = "block";
-}*/
-
 // Function for closing modals
 function closeModal() {
   navbarModal.style.display = "none";
@@ -53,19 +40,13 @@ function closeScoreModal() {
     scoreModal.style.display = "none";
 }
 
-/*function closeLoseModal() {
-    loseModal.style.display = "none";
-}*/
-
 // Function for closing click outside of modal box
 function clickOutsideModal(event) {
   if (event.target == navbarModal) {
     navbarModal.style.display = "none";
+    console.log('clicked');
   }
   if (event.target == scoreModal) {
     scoreModal.style.display = "none";
   }
-  /*if (event.target == loseModal) {
-    loseModal.style.display = "none";
-  }*/
 }
